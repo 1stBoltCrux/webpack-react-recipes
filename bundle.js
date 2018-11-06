d0b6a1a7bb75657a4cdb0fb9ed39eea1a9d77361
tedLeary <markeleary@gmail.com>
2018-11-06 14:36:28 -0800

    yarn and babel setup

---
+++ bundle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var log = function log(string) {
+  console.log(string);
+};
+
+console.log('hello world');
+
+log('hello world');
---
+++ index.js
@@ -0,0 +1,9 @@
+import path from 'path'
+
+const log = (string) => {
+  console.log(string);
+}
+
+console.log('hello world');
+
+log('hello world')
